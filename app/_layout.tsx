30d488fca02fd3ab6c8548d5cc7c4803684e8a3e
Kubrakara <kubra26kara@gmail.com>
2025-04-05 22:09:15 +0300

    feat: Implement push notification context and registration logic

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,25 @@
 import { Stack } from "expo-router";
 import "../global.css";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { NotificationProvider } from "@/context/NotificationsContext";
+import * as Notifications from "expo-notifications";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
 export default function RootLayout() {
   return (
     <>
-      <SafeAreaProvider>
-        <Stack screenOptions={{ headerShown: false }} />
-      </SafeAreaProvider>
+      <NotificationProvider>
+        <SafeAreaProvider>
+          <Stack screenOptions={{ headerShown: false }} />
+        </SafeAreaProvider>
+      </NotificationProvider>
     </>
   );
 }
